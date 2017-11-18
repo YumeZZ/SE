175a6b1516bb2ed0bd34e7d0e5b3f648560a180f
yumezz <s104213052@mail1.ncnu.edu.tw>
2017-11-18 20:40:58 +0800

    only owner can edit themselves comment

--- editMessageForm.php
+++ editMessageForm.php
@@ -8,13 +8,19 @@
     $sql = "select * from book where id = $id;";
     //執行SQL指令, 失敗則顯示無法獲取資訊
     $result = mysqli_query($conn, $sql) or die("DB Error: Cannot retrieve message.");
+    
     //如果有資料則將書名、推薦訊息、作者存入變數
     if ($rs = mysqli_fetch_assoc($result)) {
         $title = $rs['title'];
         $msg = $rs['msg'];
         $author = $rs['author'];
+        $uID = $rs['uID'];
     } else {
         echo "錯誤的書單編號(id)";
+        exit(0);
+    }
+    if (($_SESSION['uID']) != $uID) {
+        echo "You can't edit this";
         exit(0);
     }
 ?>
@@ -25,7 +31,8 @@
 <title>修改</title>
 </head>
 <body>
-<h1>編輯推薦內容: #<?php echo $id;?></h1>
+    <a href="view.php">[回推薦列表]</a>
+    <h1>編輯推薦內容: #<?php echo $id;?></h1>
     <form method="post" action="control.php?act=update">
         <input type="hidden" name='id' value="<?php echo $id;?>">
         <!-- 將推薦書單的編號(id)以隱藏的input元素藏在Form裡面送出 -->
